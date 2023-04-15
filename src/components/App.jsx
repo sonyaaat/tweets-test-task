62e97bcbdf8996475b5e07521c85537ab06bfad6
sonyaaat <sofiatkach2004@gmail.com>
2023-04-16 00:44:20 +0300

    Fixed routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
-
-import List from "./List/List";
-import { Routes, Route } from "react-router-dom";
-import Header from "./Header/Header";
-import Home from "./Home/Home";
+import List from './List/List';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './Header/Header';
+import Home from './Home/Home';
 export const App = () => {
   return (
-   <div className="container">
-   <Header/>
-   <Routes>
-   <Route path="/tweets" element={<List/>} />
-   <Route path="/main" element={<Home />} />
-   </Routes>
-
-   </div>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/tweets" element={<List />} />
+        <Route path="/main" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
